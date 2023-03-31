9bb9d9a714f1147859926b254eacb622ca2f269b
Keks <keks@htmlacademy.ru>
2023-03-31 16:45:30 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/picture.js
@@ -0,0 +1,23 @@
+const pictureContainer = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const createPictures = (pictures) => {
+  pictureContainer.querySelectorAll('.picture').forEach((element) => element.remove());
+  const pictureContainerFragment = document.createDocumentFragment();
+
+  pictures.forEach(({url, description, comments, likes, id}) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureElement.querySelector('.picture__img').alt = description;
+    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.dataset.pictureElementId = id;
+    pictureContainer.append(pictureElement);
+  });
+
+  pictureContainer.append(pictureContainerFragment);
+};
+
+export {createPictures};
---
+++ 12/js/util.js
@@ -0,0 +1,110 @@
+const ALERT_SHOW_TIME = 5000;
+const RANDOM_PHOTO_COUNT = 10;
+const Sorter = {
+  DEFAULT: 'filter-default',
+  RANDOM: 'filter-random',
+  DISCUSSED: 'filter-discussed'
+};
+
+let currentSorter = Sorter.DEFAULT;
+let pictures = [];
+
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const createIdGenerator = () => {
+  let lastGeneratedId = 0;
+
+  return function () {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const isEnterKey = (evt) => evt.key === 'Enter';
+
+function debounce (callback, timeoutDelay = 500) {
+  // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
+  // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
+  let timeoutId;
+
+  return (...rest) => {
+    // Перед каждым новым вызовом удаляем предыдущий таймаут,
+    // чтобы они не накапливались
+    clearTimeout(timeoutId);
+
+    // Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+
+    // Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
+    // пока действие совершается чаще, чем переданная задержка timeoutDelay
+  };
+}
+
+const sortRandom = () => Math.random() - 0.5;
+
+const sortByComments = (pictureA, pictureB) => pictureB.comments.length - pictureA.comments.length;
+
+const getSorteredPhotos = () => {
+  switch (currentSorter) {
+    case Sorter.RANDOM:
+      return [...pictures].sort(sortRandom).slice(0, RANDOM_PHOTO_COUNT);
+    case Sorter.DISCUSSED:
+      return [...pictures].sort(sortByComments);
+    default:
+      return [...pictures];
+  }
+};
+
+const setOnSorterClick = (cb) => {
+  const imgFilters = document.querySelector('.img-filters');
+  imgFilters.addEventListener('click', (evt) => {
+    if (!evt.target.classList.contains('img-filters__button')) {
+      return;
+    }
+    const clickedButton = evt.target;
+    if (clickedButton.id === currentSorter) {
+      return;
+    }
+    imgFilters.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+    clickedButton.classList.add('img-filters__button--active');
+    currentSorter = clickedButton.id;
+    cb(getSorteredPhotos());
+  });
+};
+
+const init = (data, cb) => {
+  pictures = [...data];
+  setOnSorterClick(cb);
+};
+
+export {getRandomInteger, createIdGenerator, getRandomArrayElement, isEscapeKey, isEnterKey, showAlert, getSorteredPhotos, init, debounce};
